98264452f9777cb862953fc1f6127c13c721704f
hahs <alexandre_2420@outlook.com>
2022-09-11 15:35:30 -0500

    custom bar

--- src/app/charts/pages/bars/bars.component.ts
+++ src/app/charts/pages/bars/bars.component.ts
@@ -28,22 +28,40 @@ export class BarsComponent {
   public barChartData: ChartData<'bar'> = {
     labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        backgroundColor: 'tomato',
+      },
       { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
     ],
   };
 
   public randomize(): void {
+    this.barChartData.datasets.forEach((bar) => {
+      bar.data = bar.data?.map(() => Math.round(Math.random() * 100));
+    });
+
     // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40,
-    ];
+    // this.barChartData.datasets[0].data = [
+    //   Math.round(Math.random() * 100),
+    //   59,
+    //   80,
+    //   Math.round(Math.random() * 100),
+    //   56,
+    //   Math.round(Math.random() * 100),
+    //   40,
+    // ];
+
+    // this.barChartData.datasets[1].data = [
+    //   Math.round(Math.random() * 100),
+    //   59,
+    //   80,
+    //   Math.round(Math.random() * 100),
+    //   56,
+    //   Math.round(Math.random() * 100),
+    //   40,
+    // ];
 
     this.chart?.update();
   }
