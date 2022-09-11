e81acd46a9de060711cce5dacd0a04f5d32113a9
hahs <alexandre_2420@outlook.com>
2022-09-11 14:38:14 -0500

    app menu

--- src/app/charts/charts-routing.module.ts
+++ src/app/charts/charts-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarsComponent } from './pages/bars/bars.component';
+import { DoubleBarsComponent } from './pages/double-bars/double-bars.component';
+import { DonutComponent } from './pages/donut/donut.component';
+import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'bar',
+        component: BarsComponent,
+      },
+      {
+        path: 'double-bar',
+        component: DoubleBarsComponent,
+      },
+      {
+        path: 'donut',
+        component: DonutComponent,
+      },
+      {
+        path: 'donut-http',
+        component: DonutHttpComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'bar',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ChartsRoutingModule { }
+export class ChartsRoutingModule {}
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  route: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+  menuItems: MenuItem[] = [
+    {
+      route: '/charts/bar',
+      name: 'Bars',
+    },
+    {
+      route: '/charts/double-bar',
+      name: 'Double Bar',
+    },
+    {
+      route: '/charts/donut',
+      name: 'Donut',
+    },
+    {
+      route: '/charts/donut-http',
+      name: 'Donut Http',
+    },
+  ];
 }
+
+// => 333
