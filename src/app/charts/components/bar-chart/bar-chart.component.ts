df8fdfaee6a640501fc2b3c35ac0c74226edd075
hahs <alexandre_2420@outlook.com>
2022-09-11 16:39:55 -0500

    custom bar inputs

--- src/app/charts/components/bar-chart/bar-chart.component.ts
+++ src/app/charts/components/bar-chart/bar-chart.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+// documentacion de charts
+//https://valor-software.com/ng2-charts/#BarChart
+
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.css']
+  styleUrls: ['./bar-chart.component.css'],
 })
 export class BarChartComponent implements OnInit {
+  @Input() horizontal: boolean = false;
 
-  constructor() { }
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    indexAxis: 'x',
+  };
+
+  barChartType: ChartType = 'bar';
+
+  @Input() barChartData!: ChartData<'bar'>;
 
   ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartOptions!.indexAxis = 'y';
+      this.barChartOptions!.scales!['y']!.min = 0;
+    }
   }
 
+  public randomize(): void {
+    this.barChartData.datasets.forEach((bar) => {
+      bar.data = bar.data?.map(() => Math.round(Math.random() * 100));
+    });
+
+    // Only Change 3 values
+    // this.barChartData.datasets[0].data = [
+    //   Math.round(Math.random() * 100),
+    //   59,
+    //   80,
+    //   Math.round(Math.random() * 100),
+    //   56,
+    //   Math.round(Math.random() * 100),
+    //   40,
+    // ];
+
+    // this.barChartData.datasets[1].data = [
+    //   Math.round(Math.random() * 100),
+    //   59,
+    //   80,
+    //   Math.round(Math.random() * 100),
+    //   56,
+    //   Math.round(Math.random() * 100),
+    //   40,
+    // ];
+
+    this.chart?.update();
+  }
 }
--- src/app/charts/pages/double-bars/double-bars.component.ts
+++ src/app/charts/pages/double-bars/double-bars.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChartData, ChartType } from 'chart.js';
 @Component({
   selector: 'app-double-bars',
   templateUrl: './double-bars.component.html',
-  styleUrls: ['./double-bars.component.css']
+  styleUrls: ['./double-bars.component.css'],
 })
-export class DoubleBarsComponent implements OnInit {
+export class DoubleBarsComponent {
+  barChartType: ChartType = 'bar';
 
-  constructor() { }
+  proveedoresData: ChartData<'bar'> = {
+    labels: ['2021', '2022', '2023', '2024', '2025'],
+    datasets: [
+      {
+        data: [100, 200, 300, 400, 500],
+        label: 'Vendedor A',
+        backgroundColor: 'tomato',
+      },
+      { data: [50, 250, 30, 450, 200], label: 'Series B' },
+    ],
+  };
 
-  ngOnInit(): void {
-  }
-
+  productoData: ChartData<'bar'> = {
+    labels: ['2021', '2022', '2023', '2024', '2025'],
+    datasets: [
+      {
+        data: [200, 300, 400, 300, 100],
+        label: 'Carros',
+        backgroundColor: 'blue',
+      },
+    ],
+  };
 }
