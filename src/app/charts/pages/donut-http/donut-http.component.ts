092c7ebeeb552807ea4aa8d7c7396b83e45f5a89
hahs <alexandre_2420@outlook.com>
2022-09-12 09:14:17 -0500

    doughnut service

--- src/app/charts/pages/donut-http/donut-http.component.ts
+++ src/app/charts/pages/donut-http/donut-http.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
+import { ChartsServiceService } from '../../services/charts-service.service';
 
 @Component({
   selector: 'app-donut-http',
   templateUrl: './donut-http.component.html',
-  styleUrls: ['./donut-http.component.css']
+  styleUrls: ['./donut-http.component.css'],
 })
 export class DonutHttpComponent implements OnInit {
+  doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
 
-  constructor() { }
+  doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      //{ data: [350, 450, 100], backgroundColor: ['tomato', 'blue', 'salmon'] },
+      { data: [] },
+    ],
+  };
+
+  doughnutChartType: ChartType = 'doughnut';
+
+  constructor(private chartService: ChartsServiceService) {}
 
   ngOnInit(): void {
+    this.chartService.getChartData().subscribe(console.log);
   }
-
 }
--- src/app/charts/services/charts-service.service.ts
+++ src/app/charts/services/charts-service.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChartsServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getChartData() {
+    return this.http.get('http://localhost:3000/charts');
+  }
 }
